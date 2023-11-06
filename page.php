28af22b91224a5bf422f9db9b00678763146b097
abdessamad <wourkout123@gmail.com>
2023-11-06 19:38:19 +0100

    optimize

--- page.php
+++ page.php
@@ -15,6 +15,7 @@
     <div class="container container--narrow page-section">
     <?php 
     $parent = wp_get_post_parent_id(get_the_ID());
+
     if($parent) {?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
@@ -22,22 +23,17 @@
         </p>
       </div>
       <?php }?>
-<?php if(get_the_ID() == 20) {?>
       <div class="page-links">
         <h2 class="page-links__title"><a href="<?php echo site_url("/about-us") ?>">About Us</a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="<?php echo site_url("/about-us/our-history") ?>">Our History</a></li>
-          <li><a href="<?php echo site_url("/about-us/our-goals") ?>">Our Goals</a></li>
+          <?php wp_list_pages(array(
+            'title_li'=> NULL,
+            'child_of' => get_the_ID()
+
+          )) ?>
         </ul>
       </div>
-<?php } else if(get_the_ID() == 3){?>
-    <div class="page-links">
-        <h2 class="page-links__title"><a href="<?php echo site_url("/privacy-policy") ?>">Privacy</a></h2>
-        <ul class="min-list">
-          <li><a href="<?php echo site_url("/privacy-policy/our-privacy") ?>">Our Privacy</a></li>
-        </ul>
-      </div>
-    <?php } ?>
+
       <div class="generic-content">
         <?php the_content(); ?>
       </div>
